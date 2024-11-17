943ba090bb50be9731686f6eb15c763411a0534d
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-17 12:20:40 +0500

    New Changes Updates Successfully

--- My Project/Controller/Studentcontroller.js
+++ My Project/Controller/Studentcontroller.js
@@ -1,27 +1,27 @@
 const addStudent =(req , res )=>{
 try{
-return res.sned(console.log("New Student Added Successfully"));
+return res.send(console.log("New Student Added Successfully"));
 }catch(error){
  return console.log(error.message)
 }
 }
 const getStudent=(req ,res)=>{
 try{
-  
+    return res.send(console.log("Get the new  Student Successfully"));
 }catch(error){
     return console.log(error.message)
 }
 }
 const updateStudent =(req ,res)=>{
    try{
-    return res.sned(console.log("Update student Successfully"))
+    return res.send(console.log("Update student Successfully"))
    }catch(error){
    return  console.log(error.message)
    }
 }
 const DeleteStudent =(req ,res)=>{
 try{
-    return res.sned(console.log("New User Deleted Successfully"))
+    return res.send(console.log("New User Deleted Successfully"))
 }catch(error){
    return console.log(error.message)
 }
--- My Project/app.js
+++ My Project/app.js
@@ -6,6 +6,7 @@ const app  = express() ;
 
 const port = 3001 ;
 
+app.use("/students" , studentroutes)
 app.listen(port,()=>{
     console.log(`Testing The Programm at At the ${port}`)
 })
