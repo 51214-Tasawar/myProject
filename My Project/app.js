7ff583fca92c9902d3a20b236d3304eeb4829aae
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-18 07:31:37 +0500

    Admin Middle Wear Completed

--- My Project/app.js
+++ My Project/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const studentroutes = require("./routes/Student")
 const TeacherRout = require("./routes/Teacher") 
+const adminRout = require("./routes/Admin")
 
 const app  = express() ;
 
@@ -9,6 +10,7 @@ const port = 3001 ;
 
 app.use("/students" , studentroutes);
 app.use("/Teachers" , TeacherRout);
+app.use ("/admin" , adminRout);
 
 app.listen(port,()=>{
     console.log(`Testing The Programm at At the ${port}`)
