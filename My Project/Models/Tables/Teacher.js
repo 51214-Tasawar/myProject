c2102a7bad34ee3a707538488a5206a6776a8f71
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-20 20:23:21 +0500

    Create Teacher Table

--- My Project/Models/Tables/Teacher.js
+++ My Project/Models/Tables/Teacher.js
@@ -1,1 +1,30 @@
 const {DataTypes , Model} = require("sequelize") 
+
+class TeacherTable extends Model { } ;
+
+TeacherTable.init(
+    {
+        TeacherId :{
+            primaryKey : true ,
+            type : DataTypes.STRING
+        },
+        Teachername: {
+            allowNull : false ,
+            type : DataTypes.STRING
+        },
+        Teacheremail :{
+            allowNull: false ,
+            unique : true ,
+            type : DataTypes.STRING
+        },
+        password : {
+            allowNull : false ,
+            type : DataTypes.STRING
+        }
+    } ,
+    {
+        name : "TeacherTable",
+        paranoid : true ,
+        timestamps: true
+    }
+)
