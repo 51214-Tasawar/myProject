2ecaa8bf42c1c3faab4758ff9a39efe0eca0bdab
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-22 20:07:27 +0500

    New Code For Student Table

--- My Project/Models/Tables/Student.js
+++ My Project/Models/Tables/Student.js
@@ -1,37 +1,28 @@
-const { DataTypes , Model } = require("sequelize") ;
+const {DataTypes , Model} = require("sequelize");
 
-const connection = require("../../dbconnection") ;
+const connection = require("../../dbconnection");
 
-class Students extends Model { } ;
+class Students extends Model { };
 
-Students.init({
-
-    studentId :{
-     primaryKey : true ,
-     type :DataTypes.STRING
-    },
-    studentname : {
-        allowNull : false ,
-        unique : true ,
-        type : DataTypes.STRING
-    },
-    studentemail : {
-        unique : true ,
-        allowNull :false ,
-        type : DataTypes.STRING,
-    } ,
-    password : {
-        allowNull : false ,
-        type : DataTypes.STRING 
-    }
-} ,
-{
-    name : "Students",
-    paranoid : true ,
-    timestamps : true,
-    sequelize : connection 
-   
-}
-)
-
-module.exports = Students
+Students.init(
+    {
+        studentId : {
+            primaryKey : true ,
+            type : DataTypes.STRING(100)
+        },
+        studentname:{
+            unique : true ,
+            type : DataTypes.STRING(10),
+            allowNull : false
+        },
+        studentemail: {
+            allowNull : false ,
+            type : DataTypes.STRING(100),
+        },
+        password  :{
+            allowNull : false ,
+            type : DataTypes.STRING(1000)
+        }
+    },{
+        name : "Students"
+    })
