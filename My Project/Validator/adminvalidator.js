f5bb05067171579839aa60cec54eaebb301898a4
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-26 08:37:21 +0500

    Validatiion Scheme Complete For admin

--- My Project/Validator/adminvalidator.js
+++ My Project/Validator/adminvalidator.js
@@ -2,5 +2,33 @@ const joi = require("joi") ;
 
 const SignUpScheme = joi.object({
     name : joi.string().min(8).max(45).required(),
-   username : joi.string().min(8).max(40).required()
+   username : joi.string().min(8).max(40).required(),
+   password : joi.string().min(8).max(50).required()
+}) 
+const login_outScheme = joi.object({
+    username : joi.string().min(8).max(40).required(),
+   password : joi.string().min(8).max(50).required()
 })
+
+module.exports ={
+  SignUp :async(req , res , next)=>{
+   try{
+  await SignUpScheme.validateAsync(req.body)
+     next()
+   }catch(error){
+  return res.send( {
+    error : error.message
+  })
+   }
+  } ,
+  log_in_out:async(req , res , nex)=>{
+    try{
+        await login_outScheme.validateAsync(req.query)
+           next()
+         }catch(error){
+        return res.send( {
+          error : error.message
+        })
+         }
+  }
+}
